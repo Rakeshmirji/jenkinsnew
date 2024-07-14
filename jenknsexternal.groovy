eedb6422c1c0b174d42ea0c0e3accba2c868d5b4
Rakesh <rakeshmirji.786@gmail.com>
2024-07-14 18:48:56 +0530

    first commit

---
+++ jenknsexternal.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent any
+
+    parameters {
+        string(name: 'name', defaultValue: 'World', description: 'Your name')
+        string(name: 'address', defaultValue: 'Earth', description: 'Your address')
+    }
+
+    stages {
+        stage('checkout') {
+            steps {
+                checkout scm(
+                    git (
+                        branches: [[name: '*/main']],
+                        extensions: [],
+                        userRemoteConfigs: [[
+                            url: 'https://github.com/Rakeshmirji/jenkinsnew'
+                        ]]
+                    )
+                )
+            }
+        } 
+        stage('build') {
+            steps {
+                bat 'python pythonsamplecode.py'
+                echo "hello how are you ${params.name} and address ${params.address}"
+            }
+        }
+    }
+}
