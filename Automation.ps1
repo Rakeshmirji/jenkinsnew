04817e8707bf4a91351f0d66ec0bf0f355acf782
Rakesh Mirji <Rakesh.Mirji1@citrix.com>
2024-07-18 17:26:32 +0530

    saas

---
+++ Automation.ps1
@@ -0,0 +1,31 @@
+#
+# Automation.ps1
+#
+
+### Get present working directory and parent working directory ###
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$hereParent = Split-Path -Path $here -Parent
+Write-Host "here = $here"
+Write-Host "hereParent = $hereParent"
+
+#### Import powershell files for accessing functions in these files ######
+. "$hereParent\Common\RfWin_Support_Functions.ps1"
+. "$hereParent\Common\CommonOperations.ps1"
+. "$hereParent\Common\ScreenElementsOperations\InstallerScreen.ps1"
+. "$hereParent\Common\ScreenElementsOperations\sspWindowScreen.ps1"
+. "$hereParent\Common\ScreenElementsOperations\DiagnosticToolScreen.ps1"
+
+### Convert Json to array list to get values #####
+$json = (Get-Content "$here\Automation1.json" -Raw) | ConvertFrom-Json
+$Global:jsondata = $json
+#### Read Values supplied in JSON file ######
+### CWA Values ###
+write-host "the json is $json"
+[string]$Global:StoreTypeX1 = $json.CWA_values.Store_TypeX1
+[string]$Global:StoreURLX1 = $json.CWA_values.Store_URLX1
+[string]$Global:StoreNameNSG = $json.CWA_Values.Store_NameNSG
+[string]$Global:StoreName = $json.CWA_values.Store_NameX1
+[string]$Global:StoreURLNSG = $json.CWA_Values.Store_URLNSG
+[string]$Global:StoreTypeNSG = $json.CWA_values.Store_TypeNSG
+#[string]$Global:TestUserName = $json.CWA_values.Test_User_Name
+#[string]$Global:OnpremPass = $json.CWA_Values.Test_User_Password.plainText
