0df5b80561cc0fe44dcd41e6065520e7241b17fe
Rakesh Mirji <Rakesh.Mirji1@citrix.com>
2024-07-20 23:24:37 +0530

    Appp

--- Automation.ps1
+++ Automation.ps1
@@ -24,17 +24,18 @@ Write-Host "The JSON is:"
 Write-Host $Global:jsondata
 # #### Read Values supplied in JSON file ######
 # ### CWA Values ###
-write-host "the json is $json"
-[string]$Global:StoreTypeX1 = $json.CWA_values.Store_TypeX1
-[string]$Global:StoreURLX1 = $json.CWA_values.Store_URLX1
-[string]$Global:StoreNameNSG = $json.CWA_Values.Store_NameNSG
-[string]$Global:StoreName = $json.CWA_values.Store_NameX1
-[string]$Global:StoreURLNSG = $json.CWA_Values.Store_URLNSG
-[string]$Global:StoreTypeNSG = $json.CWA_values.Store_TypeNSG
+
+[string]$Global:StoreTypeX1 = $jsondata.CWA_values.Store_TypeX1
+[string]$Global:StoreURLX1 = $jsondata.CWA_values.Store_URLX1
+[string]$Global:StoreNameNSG = $jsondata.CWA_Values.Store_NameNSG
+[string]$Global:StoreName = $jsondata.CWA_values.Store_NameX1
+[string]$Global:StoreURLNSG = $jsondata.CWA_Values.Store_URLNSG
+[string]$Global:StoreTypeNSG = $jsondata.CWA_values.Store_TypeNSG
 #[string]$Global:TestUserName = $json.CWA_values.Test_User_Name
 #[string]$Global:OnpremPass = $json.CWA_Values.Test_User_Password.plainText
-[string]$Global:StoreTypeX2 = $json.CWA_values.Test_Suite_Name
-[string]$Global:StoreTypeX1 = $json.Test_Suite_Name
+[string]$Global:StoreTypeX2 = $jsondata.CWA_values.Test_Suite_Name
+[string]$Global:StoreTypeX1 = $jsondata.Test_Suite_Name
+
 Write-Host "fffffffffffffffff"
 Write-Host $StoreTypeX1
 Write-Host "fffffffffffffffff"
