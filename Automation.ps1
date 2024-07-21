cb0b2d41b77cb353f7fa9b2481ec7ac35833f546
Rakesh Mirji <Rakesh.Mirji1@citrix.com>
2024-07-21 22:05:23 +0530

    hjgjh

--- Automation.ps1
+++ Automation.ps1
@@ -1,6 +1,48 @@
-#
+# #
+# # Automation.ps1
+# #
+
+# ### Get present working directory and parent working directory ###
+# $here = Split-Path -Parent $MyInvocation.MyCommand.Path
+# $hereParent = Split-Path -Path $here -Parent
+# Write-Host "here = $here"
+# Write-Host "hereParent = $hereParent"
+
+
+
+# ### Convert Json to array list to get values #####
+# # $json = (Get-Content "$here\Automation1.json" -Raw) | ConvertFrom-Json
+# # $Global:jsondata = $json
+# # #$json = (Get-Content "$here\Automation1.json" -Raw) | ConvertFrom-Json
+# # $Global:jsondata = $json | Format-Table -AutoSize
+# # write-host "the jsondata is $Global:jsondata"
+
+# $json = (Get-Content "$here\automation1.json" -Raw) | ConvertFrom-Json
+# $Global:jsondata = $json | ConvertTo-Json -Depth 100
+# #$Global:jsondata = $json
+# #### Read Values supplied in JSON file ######
+# ### CWA Values ###
+# write-host "the json is $jsondata"
+# [string]$Global:StoreTypeX1 = $json.CWA_values.Store_TypeX1
+# [string]$Global:StoreURLX1 = $json.CWA_values.Store_URLX1
+# [string]$Global:StoreNameNSG = $json.CWA_Values.Store_NameNSG
+# [string]$Global:StoreName = $json.CWA_values.Store_NameX1
+# [string]$Global:StoreURLNSG = $json.CWA_Values.Store_URLNSG
+# [string]$Global:StoreTypeNSG = $json.CWA_values.Store_TypeNSG
+# #[string]$Global:TestUserName = $json.CWA_values.Test_User_Name
+# #[string]$Global:OnpremPass = $json.CWA_Values.Test_User_Password.plainText
+# [string]$Global:BuildBranchName = $json.CWA_values.BuildBranchName
+
+# write-host "the StoreTypeX1 is $Global:StoreTypeX1"
+# write-host "the StoreURLX1 is $Global:StoreURLX1"
+# write-host "the StoreNameNSG is $Global:StoreNameNSG"
+# write-host "the StoreName is $Global:StoreName"
+# write-host "the StoreURLNSG is $Global:StoreURLNSG"
+# write-host "the StoreTypeNSG is $Global:StoreTypeNSG"
+# #write-host "the TestUserName is $Global:TestUserName"
+# #write-host "the OnpremPass is $Global:OnpremPass"  
+# write-host "the BuildBranchName is $Global:BuildBranchName"
 # Automation.ps1
-#
 
 ### Get present working directory and parent working directory ###
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
@@ -8,30 +50,20 @@ $hereParent = Split-Path -Path $here -Parent
 Write-Host "here = $here"
 Write-Host "hereParent = $hereParent"
 
-
-
 ### Convert Json to array list to get values #####
-# $json = (Get-Content "$here\Automation1.json" -Raw) | ConvertFrom-Json
-# $Global:jsondata = $json
-# #$json = (Get-Content "$here\Automation1.json" -Raw) | ConvertFrom-Json
-# $Global:jsondata = $json | Format-Table -AutoSize
-# write-host "the jsondata is $Global:jsondata"
-
 $json = (Get-Content "$here\automation1.json" -Raw) | ConvertFrom-Json
-$Global:jsondata = $json | ConvertTo-Json -Depth 100
-#$Global:jsondata = $json
+$Global:jsondata = $json
+
 #### Read Values supplied in JSON file ######
 ### CWA Values ###
 write-host "the json is $jsondata"
-[string]$Global:StoreTypeX1 = $json.CWA_values.Store_TypeX1
-[string]$Global:StoreURLX1 = $json.CWA_values.Store_URLX1
-[string]$Global:StoreNameNSG = $json.CWA_Values.Store_NameNSG
-[string]$Global:StoreName = $json.CWA_values.Store_NameX1
-[string]$Global:StoreURLNSG = $json.CWA_Values.Store_URLNSG
-[string]$Global:StoreTypeNSG = $json.CWA_values.Store_TypeNSG
-#[string]$Global:TestUserName = $json.CWA_values.Test_User_Name
-#[string]$Global:OnpremPass = $json.CWA_Values.Test_User_Password.plainText
-[string]$Global:BuildBranchName = $json.CWA_values.BuildBranchName
+[string]$Global:StoreTypeX1 = $jsondata.CWA_values.Store_TypeX1
+[string]$Global:StoreURLX1 = $jsondata.CWA_values.Store_URLX1
+[string]$Global:StoreNameNSG = $jsondata.CWA_values.Store_NameNSG
+[string]$Global:StoreName = $jsondata.CWA_values.Store_NameX1
+[string]$Global:StoreURLNSG = $jsondata.CWA_values.Store_URLNSG
+[string]$Global:StoreTypeNSG = $jsondata.CWA_values.Store_TypeNSG
+[string]$Global:BuildBranchName = $jsondata.CWA_values.BuildBranchName
 
 write-host "the StoreTypeX1 is $Global:StoreTypeX1"
 write-host "the StoreURLX1 is $Global:StoreURLX1"
@@ -39,6 +71,4 @@ write-host "the StoreNameNSG is $Global:StoreNameNSG"
 write-host "the StoreName is $Global:StoreName"
 write-host "the StoreURLNSG is $Global:StoreURLNSG"
 write-host "the StoreTypeNSG is $Global:StoreTypeNSG"
-#write-host "the TestUserName is $Global:TestUserName"
-#write-host "the OnpremPass is $Global:OnpremPass"  
 write-host "the BuildBranchName is $Global:BuildBranchName"
