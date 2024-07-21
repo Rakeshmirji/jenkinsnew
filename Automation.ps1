37a344c48fb17fa052ead8f947ec01e384c185d3
Rakesh Mirji <Rakesh.Mirji1@citrix.com>
2024-07-21 22:09:32 +0530

    hjgjh

--- Automation.ps1
+++ Automation.ps1
@@ -45,25 +45,27 @@
 # Automation.ps1
 
 ### Get present working directory and parent working directory ###
+# Automation.ps1
+
+### Get present working directory and parent working directory ###
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $hereParent = Split-Path -Path $here -Parent
 Write-Host "here = $here"
 Write-Host "hereParent = $hereParent"
 
 ### Convert Json to array list to get values #####
-$json = (Get-Content "$here\automation1.json" -Raw) | ConvertFrom-Json
-$Global:jsondata = $json
+$json = Get-Content "$here\automation1.json" -Raw | ConvertFrom-Json
 
 #### Read Values supplied in JSON file ######
 ### CWA Values ###
-write-host "the json is $jsondata"
-[string]$Global:StoreTypeX1 = $jsondata.CWA_values.Store_TypeX1
-[string]$Global:StoreURLX1 = $jsondata.CWA_values.Store_URLX1
-[string]$Global:StoreNameNSG = $jsondata.CWA_values.Store_NameNSG
-[string]$Global:StoreName = $jsondata.CWA_values.Store_NameX1
-[string]$Global:StoreURLNSG = $jsondata.CWA_values.Store_URLNSG
-[string]$Global:StoreTypeNSG = $jsondata.CWA_values.Store_TypeNSG
-[string]$Global:BuildBranchName = $jsondata.CWA_values.BuildBranchName
+write-host "the json is $json"
+[string]$Global:StoreTypeX1 = $json.CWA_values.Store_TypeX1
+[string]$Global:StoreURLX1 = $json.CWA_values.Store_URLX1
+[string]$Global:StoreNameNSG = $json.CWA_values.Store_NameNSG
+[string]$Global:StoreName = $json.CWA_values.Store_NameX1
+[string]$Global:StoreURLNSG = $json.CWA_values.Store_URLNSG
+[string]$Global:StoreTypeNSG = $json.CWA_values.Store_TypeNSG
+[string]$Global:BuildBranchName = $json.CWA_values.BuildBranchName
 
 write-host "the StoreTypeX1 is $Global:StoreTypeX1"
 write-host "the StoreURLX1 is $Global:StoreURLX1"
