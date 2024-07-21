588d3568a550d6b8a2da9e1d16369d8ba19da649
Rakesh Mirji <Rakesh.Mirji1@citrix.com>
2024-07-21 21:03:54 +0530

    hjgjh

--- Automation.ps1
+++ Automation.ps1
@@ -26,7 +26,8 @@ Write-Host $Global:jsondata
 Write-Host $StoreURLNSG
 # #### Read Values supplied in JSON file ######
 # ### CWA Values ###
-$jsondata=(ConvertFrom-Json $jsondata)
+#$jsondata=(ConvertFrom-Json $jsondata)
+$jsondata = ConvertFrom-Json $jsondata
 [string]$Global:StoreTypeX1 = $jsondata.CWA_values.Store_TypeX1
 [string]$Global:StoreURLX1 = $jsondata.CWA_values.Store_URLX1
 [string]$Global:StoreNameNSG = $jsondata.CWA_values.Store_NameNSG
@@ -37,14 +38,23 @@ $jsondata=(ConvertFrom-Json $jsondata)
 #[string]$Global:OnpremPass = $json.CWA_Values.Test_User_Password.plainText
 [string]$Global:StoreTypeX2 = $jsondata.CWA_values.Test_Suite_Name
 [string]$Global:StoreTypeX1 = $jsondata.Test_Suite_Name
+Write-Host $StoreNameNSG
+Write-Host $StoreURLNSG
+Write-Host $StoreNameNSG
+Write-Host $StoreTypeX1
 #Write-Host $jsondata.CWA_values.ResourceName
 Write-Host "fffffffffffffffff"
-
+$jsondata = (ConvertFrom-Json $jsondata)
+[string]$Global:StoreNameNSG = $jsondata.CWA_values.Store_NameNSG
+Write-Host $StoreNameNSG
 Write-Host $StoreURLNSG
 $resourceName = (ConvertFrom-Json $jsondata).CWA_values.ResourceName
 Write-Host $resourceName
 #Write-Host $jsondata.CWA_values.ResourceName
 Write-Host "fffffffffffffffff"
+
+$jsondata=(ConvertFrom-Json $jsondata)
+[string]$Global:StoreNameNSG = $jsondata.CWA_values.Store_NameNSG
 Write-Host $StoreURLNSG
 #Write-Host $jsondata.CWA_values.ResourceName
 Write-Host "fffffffffffffffff"
