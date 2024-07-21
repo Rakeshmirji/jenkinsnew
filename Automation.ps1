09ce8957f5599427791a25fed7793698a6e5ae91
Rakesh Mirji <Rakesh.Mirji1@citrix.com>
2024-07-21 22:14:48 +0530

    hjgjh

--- Automation.ps1
+++ Automation.ps1
@@ -48,29 +48,38 @@
 # Automation.ps1
 
 ### Get present working directory and parent working directory ###
+# Automation.ps1
+
+### Get present working directory and parent working directory ###
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $hereParent = Split-Path -Path $here -Parent
 Write-Host "here = $here"
 Write-Host "hereParent = $hereParent"
 
 ### Convert Json to array list to get values #####
-$json = Get-Content "$here\automation1.json" -Raw | ConvertFrom-Json
+$json = (Get-Content "$here\automation1.json" -Raw) | ConvertFrom-Json
+$Global:jsondata = $json
 
 #### Read Values supplied in JSON file ######
 ### CWA Values ###
-write-host "the json is $json"
+Write-Host "the json is $jsondata"
+
 [string]$Global:StoreTypeX1 = $json.CWA_values.Store_TypeX1
 [string]$Global:StoreURLX1 = $json.CWA_values.Store_URLX1
 [string]$Global:StoreNameNSG = $json.CWA_values.Store_NameNSG
 [string]$Global:StoreName = $json.CWA_values.Store_NameX1
 [string]$Global:StoreURLNSG = $json.CWA_values.Store_URLNSG
 [string]$Global:StoreTypeNSG = $json.CWA_values.Store_TypeNSG
+#[string]$Global:TestUserName = $json.CWA_values.Test_User_Name
+#[string]$Global:OnpremPass = $json.CWA_values.Test_User_Password.plainText
 [string]$Global:BuildBranchName = $json.CWA_values.BuildBranchName
 
-write-host "the StoreTypeX1 is $Global:StoreTypeX1"
-write-host "the StoreURLX1 is $Global:StoreURLX1"
-write-host "the StoreNameNSG is $Global:StoreNameNSG"
-write-host "the StoreName is $Global:StoreName"
-write-host "the StoreURLNSG is $Global:StoreURLNSG"
-write-host "the StoreTypeNSG is $Global:StoreTypeNSG"
-write-host "the BuildBranchName is $Global:BuildBranchName"
+Write-Host "the StoreTypeX1 is $Global:StoreTypeX1"
+Write-Host "the StoreURLX1 is $Global:StoreURLX1"
+Write-Host "the StoreNameNSG is $Global:StoreNameNSG"
+Write-Host "the StoreName is $Global:StoreName"
+Write-Host "the StoreURLNSG is $Global:StoreURLNSG"
+Write-Host "the StoreTypeNSG is $Global:StoreTypeNSG"
+# Write-Host "the TestUserName is $Global:TestUserName"
+# Write-Host "the OnpremPass is $Global:OnpremPass"
+Write-Host "the BuildBranchName is $Global:BuildBranchName"
