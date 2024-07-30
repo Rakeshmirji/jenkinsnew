89839727dae6fd9a1ba4882f8cb28109f796d981
Rakesh Mirji <Rakesh.Mirji1@citrix.com>
2024-07-30 15:53:45 +0530

    commit1

--- jenknsexternal.groovy
+++ jenknsexternal.groovy
@@ -49,18 +49,58 @@ pipeline {
                     //def MY_ARRAY = System.getenv('MY_ARRAY')
                     // Now you can access the array in your Groovy code
                     //println MY_ARRAY
-                    def jsonConfigString = JsonOutput.toJson(configMap)
-                    // Convert JSON string to JSON Object
-                    def jsonConfig = readJSON text: jsonConfigString
-                    // Save config as JSON in correct dir
-                    def configPath = "${env.WORKSPACE}/automation1.json"
-                    writeJSON(file: configPath, json: jsonConfig, pretty: 4)
-                    powershellPath = "${env.WORKSPACE}/Automation.ps1"
-                    result = powershell(powershellPath)
-                    //CWA_values_map = readJSON text: CWA_values
-                    //println("Cwa_Values_onprem is ----  ${params.CWA_values_map}")
-                
-                }
+                    for (int i = 0; i < myArray.size(); i++) {
+                def value = myArray[i]
+                println "Value: $value, Type: ${value.getClass().getName()}"
+                // You can add more logic here
+            }
+
+
+
+            for (int i = 0; i < myArray.size(); i++) {
+                def value = myArray[i]
+                println "Value: $value, Type: ${value.getClass().getName()}"
+                // You can add more logic here
+            
+                def jsonConfigString = JsonOutput.toJson(configMap)
+                // Convert JSON string to JSON Object
+                def jsonConfig = readJSON text: jsonConfigString
+                // Save config as JSON in correct dir
+                def configPath = "${env.WORKSPACE}/automation1.json"
+                writeJSON(file: configPath, json: jsonConfig, pretty: 4)
+                powershellPath = "${env.WORKSPACE}/Automation.ps1"
+                result = powershell(powershellPath)
+                //CWA_values_map = readJSON text: CWA_values
+                //println("Cwa_Values_onprem is ----  ${params.CWA_values_map}")
+                    
+
+
+
+                nextDescribeToExecute = ["DescribeToExecute": i]
+                configMap << nextDescribeToExecute
+
+                jsonConfigString = JsonOutput.toJson(configMap)
+                jsonConfig = readJSON text: jsonConfigString
+                // Save config as JSON in correct dir
+                writeJSON(file: configPath, json: jsonConfig, pretty: 4)
+                powershellPath = './Automation/CWA_Automation/CWA_Automation/flows/onprem.ps1'
+                result = powershell(returnStatus: true, script: powershellPath)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+               }
+                  }
                 bat 'python ./newone/pythonsamplecode.py'
                 println "Testing ${configMap.fathername}"
                 echo "Testing ${configMap.fathername}"
